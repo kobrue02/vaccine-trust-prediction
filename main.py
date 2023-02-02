26a9150ab749abb05fc544143ad0d92991ead06c
liaaneeee <123038778+liaaneeee@users.noreply.github.com>
2023-02-02 11:48:21 +0100

    Tried to train some models
    Added a new function create_tfidf_matrix() and tried to train Support Vector Machine

--- main.py
+++ main.py
@@ -42,6 +42,15 @@ def process(sent: str):
     return result
 
 
+def create_tfidf_matrix():
+    vectorizer = CountVectorizer(ngram_range=(3, 3), stop_words='english')
+    transformer = TfidfTransformer()
+    count_matrix = vectorizer.fit_transform(dataset["text"])
+    tfidf_matrix = transformer.fit_transform(count_matrix)
+    tfidf_df = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix)
+    return tfidf_df
+
+
 data = [process(tex) for tex in df["text"].tolist() if len(tex.split()) > 1]
 
 dataset = pd.DataFrame(data, columns=["text", "lemmata", "label", "sentiment", "bigrams",
@@ -54,6 +63,61 @@ dataset = pd.DataFrame(data, columns=["text", "lemmata", "label", "sentiment", "
                                       "ngram matches for label 2",
                                       "ngram matches for label 3"])
 
+tfidf_matrix = create_tfidf_matrix()
+new_dataset = pd.concat([dataset, tfidf_matrix], axis=1)
+
+# Ablation study
+sentiment_only = ["sentiment"]
+kw_matches_only = ["kw matches for label 0",
+                   "kw matches for label 1",
+                   "kw matches for label 2",
+                   "kw matches for label 3"]
+ngram_matches_only = ["ngram matches for label 0",
+                      "ngram matches for label 1",
+                      "ngram matches for label 2",
+                      "ngram matches for label 3"]
+
+
+def train_baseline():
+    pass
+
+
+def create_data(features: list, drop):
+    if drop:
+        X = new_dataset.drop(features, axis=1)
+    else:
+        X = dataset[features]
+    X.columns = X.columns.astype(str)
+    y = new_dataset["label"]
+    # Create training and test sets
+    return train_test_split(
+        X, y, train_size=0.8, stratify=y, random_state=10)
+
+
+def train_linear_svc(features: list, drop=False):
+    X_train, X_test, y_train, y_test = create_data(features, drop)
+    # Initialize model
+    clf = LinearSVC(random_state=10)
+    # Train model
+    clf.fit(X_train, y_train)
+    # Compute accuracy
+    predicted = clf.predict(X_test)
+    # print(f"Accuracy LinearSVC: {clf.score(X_test, y_test)}")
+    print("Linear SVC:")
+    print(classification_report(y_test, predicted, zero_division=0))
+
+
+def train_svc(features: list, drop=False):
+    X_train, X_test, y_train, y_test = create_data(features, drop)
+    # Initialize model
+    clf = SVC(random_state=10)
+    # Train model
+    clf.fit(X_train, y_train)
+    # Compute accuracy
+    predicted = clf.predict(X_test)
+    # print(f"Accuracy SVC: {clf.score(X_test, y_test)}")
+    print("SVC:")
+    print(classification_report(y_test, predicted, zero_division=0))
 
 def train_model(model):
     # Ablation Study
@@ -84,4 +148,34 @@ if __name__ == "__main__":
     print(dataset.head())
     for model in [MLPClassifier(max_iter=100), BernoulliNB(), GaussianNB()]
         train_model(model)
+        
+     # print(dataset.head())
+    # train_model()
+    train_linear_svc(["text", "lemmata", "label", "bigrams"], drop=True)
+    train_linear_svc(["text", "lemmata", "label", "bigrams",
+                      "sentiment",
+                      "kw matches for label 0",
+                      "kw matches for label 1",
+                      "kw matches for label 2",
+                      "kw matches for label 3",
+                      "ngram matches for label 0",
+                      "ngram matches for label 1",
+                      "ngram matches for label 2",
+                      "ngram matches for label 3"], drop=True)
+    train_svc(["text", "lemmata", "label", "bigrams"], drop=True)
+    train_svc(["text", "lemmata", "label", "bigrams",
+               "sentiment",
+               "kw matches for label 0",
+               "kw matches for label 1",
+               "kw matches for label 2",
+               "kw matches for label 3",
+               "ngram matches for label 0",
+               "ngram matches for label 1",
+               "ngram matches for label 2",
+               "ngram matches for label 3"], drop=True)
+
+    # for i in range(0,4):
+    #    raw_text = dataset[dataset["label"] == i]["text"].tolist()
+    #    bigrams = most_common_ngrams(raw_text, 4)
+    #    print(bigrams)
 
