b28ae68acf72cce27f402ff0b8d27d48c2bae4e1
liaaneeee <thaovanliane.nguyen@yahoo.de>
2023-02-05 20:07:07 +0100

    Outsourced processing
    Outsourced processing to new file

---
+++ process.py
@@ -0,0 +1,77 @@
+import spacy
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+from sentiment_analysis import return_polarity_score
+from lexical_features import return_features, count_features
+
+nlp = spacy.load("en_core_web_sm")
+
+# Read raw data
+pd.set_option('display.max_columns', None)
+raw_df = pd.read_csv("data/train.csv")
+# Delete rows with sent length <= 1
+raw_df = raw_df[raw_df["text"].apply(lambda x: len(x.split()) > 1)]
+# Retrieve most meaningful keywords and ngrams
+label2keywords = return_features(raw_df, 20)
+label2ngrams = return_features(raw_df, 20, ngrams=True)
+# print(label2keywords)
+# print(label2ngrams)
+
+
+def preprocess(sent):
+    doc = nlp(sent)
+    lemmata = str(" ".join([t.lemma_ for t in doc]))
+    return lemmata
+
+
+def create_tfidf_df(data, ngrams=False):
+    if ngrams:
+        vectorizer = CountVectorizer(ngram_range=(3, 3), stop_words='english')
+    else:
+        vectorizer = CountVectorizer(stop_words='english')
+    transformer = TfidfTransformer()
+    count_matrix = vectorizer.fit_transform(data["text"].tolist())
+    tfidf_matrix = transformer.fit_transform(count_matrix)
+    tfidf_df = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix)
+    return tfidf_df
+
+
+def extract_features(sent):
+    # Sentiment score
+    sentiment = return_polarity_score(preprocess(sent))
+    # Keyword counts
+    kw_matches = [count_features(sent, kw_list)
+                  for kw_list in label2keywords.values()]
+    # Ngram counts
+    ngram_matches = [count_features(sent, ngram_list)
+                     for ngram_list in label2ngrams.values()]
+    feat_list = [sentiment]
+    feat_list.extend(kw_matches)
+    feat_list.extend(ngram_matches)
+    return feat_list
+
+
+# Extract own features and create dataframe
+features = [extract_features(sent) for sent in raw_df["text"]]
+df = pd.DataFrame(features, columns=[
+    "sentiment",
+    "kw matches for label 0",
+    "kw matches for label 1",
+    "kw matches for label 2",
+    "kw matches for label 3",
+    "ngram matches for label 0",
+    "ngram matches for label 1",
+    "ngram matches for label 2",
+    "ngram matches for label 3"
+])
+df["label"] = raw_df["label"].tolist()
+print(df.shape)
+# Create dataframe based on own features and tf idf on text
+df1 = pd.concat([df, create_tfidf_df(raw_df)], axis=1)
+# Create dataframe based on own features and tf idf on ngrams
+df2 = pd.concat([df, create_tfidf_df(raw_df, ngrams=True)], axis=1)
+print(df1.shape)
+print(df2.shape)
