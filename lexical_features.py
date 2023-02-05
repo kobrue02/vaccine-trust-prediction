6271db4a20323a2c6d6ec5b9ce43f1d1f71347c9
liaaneeee <123038778+liaaneeee@users.noreply.github.com>
2023-02-05 20:02:22 +0100

    Improved lexical features
    Improved functions of lexical features

--- lexical_features.py
+++ lexical_features.py
@@ -10,54 +10,54 @@ def in_blacklist(w):
     return bool(match(r"(.)*(\d)+|covid(.)*|vaccin(\w)*", w.lower()))
 
 
-def create_corpus(df):
+def create_corpus(data):
     """
     Combines all sents of one label to one document and
     creates a corpus out of all document strings"""
     corp = []
-    labels = list(set(df["label"]))
-    for label in labels:
+    index2label = dict()
+    for i, label in enumerate(list(set(data["label"]))):
         doc = ""
-        for sent in df.loc[df["label"] == label]["text"]:
+        for sent in data.loc[data["label"] == label]["text"]:
             tokens = nlp(sent)
             doc += " " + " ".join([t.lemma_ for t in tokens
                                    if not in_blacklist(t.lemma_)])
+
         corp.append(doc)
-    return corp
+        index2label[i] = label
+    return corp, index2label
 
 
-def get_sorted_indexes(df, matrix):
+def get_sorted_indexes(matrix):
     """
     Create dict with labels as keys and
     lists of indexes of sorted tfidf values as values"""
-    sorted_tfidf = {}
-    labels = list(set(df["label"]))
-    for label in labels:
-        tfidf_array = matrix.toarray()[label]
-        sorted_tfidf[label] = [
-            i for i, val in sorted(list(enumerate(tfidf_array)),
-                                   key=lambda x: x[1],
-                                   reverse=True)]
-    return sorted_tfidf
+    sorted_indexes = []
+    for i in range(len(matrix.toarray())):
+        tfidf_array = matrix.toarray()[i]
+        sorted_indexes.append(
+            [i for i, val in sorted(list(enumerate(tfidf_array)),
+                                    key=lambda x: x[1], reverse=True)]
+        )
+    return sorted_indexes
 
 
-def get_top_n_features(feat_list, sorted_idxs, n):
+def get_top_n_features(features, sorted_idxs, n):
     """
     Create dict with labels as keys and
     lists of features sorted by tfidf as values"""
-    top_features_dict = dict()
-    for label in sorted_idxs.keys():
-        top_features_dict[label] = [feat_list[i] for i in
-                                    sorted_idxs.get(label)[:n + 1]]
-    return top_features_dict
+    top_features = []
+    for idx_list in sorted_idxs:
+        top_features.append([features[i] for i in idx_list[:n + 1]])
+    return top_features
 
 
-def return_features(df, ngrams=False):
+def return_features(df, n, ngrams=False):
     """
     Takes dataframe and proceeeds through all
     steps of keyword extraction """
     # Create corpus
-    corpus = create_corpus(df)
+    corpus, index2label = create_corpus(df)
     # Use ngrams
     if ngrams:
         vectorizer = CountVectorizer(ngram_range=(3, 3), stop_words='english')
@@ -69,26 +69,24 @@ def return_features(df, ngrams=False):
     tfidf_matrix = transformer.fit_transform(count_matrix)
     features = vectorizer.get_feature_names_out()
     # Get sorted features
-    sorted_indexes = get_sorted_indexes(df, tfidf_matrix)
-    top_features = get_top_n_features(features, sorted_indexes, 30)
-    return top_features
+    sorted_indexes = get_sorted_indexes(tfidf_matrix)
+    top_features = get_top_n_features(features, sorted_indexes, n)
+    label2features = {label: top_features[i] for i, label in index2label.items()}
+    return label2features
 
 
-def count_features(sent, top_features_dict):
+def count_features(sent, feature_list):
     """
     Count occurence of keywords in sents for each label"""
-    counts = []
-    for features in top_features_dict.values():
-        c = 0
-        for ft in features:
-            c += 1 if ft in sent else 0
-        counts.append(c)
-    return counts
+    return sum([sent.count(ft) for ft in feature_list])
 
 
 if __name__ == '__main__':
     # Read CSV
     my_df = pd.read_csv("data/train.csv")
     # Extract keywords
-    ngrams = return_features(my_df, ngrams=True)
+    # keywords = return_features(my_df, 20)
+    # print(keywords)
+    # Extract ngrams
+    ngrams = return_features(my_df, 20, ngrams=True)
     print(ngrams)
